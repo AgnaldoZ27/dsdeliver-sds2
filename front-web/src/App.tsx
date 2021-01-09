f2465aaa887e86f724bc43367df77a40008d50a1
AgnaldoZ27 <agnaldojose.131@gmail.com>
2021-01-09 09:10:03 -0400

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-    <Navbar/>
-    <Home/>
-    </div>
+    <Routes />
   );
 }
 
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,31 +1,31 @@
-import Navbar from '../Navbar';
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg'
 import Footer from '../Footer';
+import {Link} from "react-router-dom"
 
 function Home() {
     return (
         <>
-        <div className="home-container">
-            <div className="homw-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido <br /> que entregamos <br /> pra você!
+            <div className="home-container">
+                <div className="homw-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br /> que entregamos <br /> pra você!
                     </h1>
-                    <h3 className="home-subtitle">
-                        Escolha o seu pedido e em poucos minutos <br />
+                        <h3 className="home-subtitle">
+                            Escolha o seu pedido e em poucos minutos <br />
                         levaremos na sua porta
                     </h3>
-                    <a href="orders" className="home-btn-order">
-                        FAZER PEDIDO
-                    </a>
-                </div>
-                <div className="home-image">
-                    <MainImage />
+                        <Link to="/orders" className="home-btn-order">
+                            FAZER PEDIDO
+                    </Link>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
             </div>
-        </div>
-        <Footer/>
+            <Footer />
         </>
     );
 }
